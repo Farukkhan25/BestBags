33655d15a1c556731760227820eff93e50a49ae9
Farukkhan25 <programmingherofaruk@gmail.com>
2022-11-24 21:07:54 +0600

    Banner added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import image1 from "../../assets/bag_images/slide1.jpg";
+import image2 from "../../assets/bag_images/slide2.jpg";
+import image3 from "../../assets/bag_images/slide3.jpg";
+import image4 from "../../assets/bag_images/banner-backpacks.jpg";
+
+const Banner = () => {
+    return (
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={image1} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={image2} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={image3} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={image4} alt="" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
-import Home from "../../Pages/Blog/Home/Home";
+import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
 
@@ -10,10 +10,10 @@ const router = createBrowserRouter([
     element: <Main></Main>,
     // errorElement: <DisplayError></DisplayError>,
     children: [
-    //   {
-    //     path: "/",
-    //     element: <Home></Home>,
-    //   },
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
       {
         path: "/login",
         element: <Login></Login>,
